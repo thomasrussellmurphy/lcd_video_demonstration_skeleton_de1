b639832ddd55ed5beb7f688a86026485c55ed117
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-10-16 17:26:20 -0400

    Updating formatting on the demonstration files

--- V/video_position_sync.v
+++ V/video_position_sync.v
@@ -1,10 +1,10 @@
 module video_position_sync
        (
-           input disp_clk,
-           input en,
-           output reg disp_hsync, disp_vsync,
-           output reg valid_draw, v_blank,
-           output reg [ 9: 0 ] h_pos, v_pos
+         input disp_clk,
+         input en,
+         output reg disp_hsync, disp_vsync,
+         output reg valid_draw, v_blank,
+         output reg [ 9: 0 ] h_pos, v_pos
        );
 // Module header template
 // Replace with implemented version to test functionality
